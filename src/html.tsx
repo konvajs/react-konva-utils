d2acbb97f81eee2adb9c90f3e43ea5f7d84579c9
Lennard Scheibel <contact@lennardscheibel.de>
2023-01-18 10:12:10 +0100

    export Html prop types

--- src/html.tsx
+++ src/html.tsx
@@ -9,7 +9,7 @@ const needForceStyle = (el: HTMLDivElement) => {
   return !ok;
 };
 
-type TransformAttrs = {
+export type HtmlTransformAttrs = {
   x: number;
   y: number;
   scaleX: number;
@@ -19,11 +19,11 @@ type TransformAttrs = {
   skewY: number;
 };
 
-type Props = PropsWithChildren<{
+export type HtmlProps = PropsWithChildren<{
   groupProps?: Konva.ContainerConfig;
   divProps?: any;
   transform?: boolean;
-  transformFunc?: (attrs: TransformAttrs) => TransformAttrs;
+  transformFunc?: (attrs: HtmlTransformAttrs) => HtmlTransformAttrs;
 }>;
 
 export const Html = ({
@@ -32,7 +32,7 @@ export const Html = ({
   divProps,
   transform,
   transformFunc,
-}: Props) => {
+}: HtmlProps) => {
   const groupRef = React.useRef<Konva.Group>(null);
   const container = React.useRef<HTMLDivElement>();
 
